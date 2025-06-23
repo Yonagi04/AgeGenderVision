5deabc98158b3baa122a6133442ddace18abbf69
Yonagi <1809266700@qq.com>
2025-06-23 11:23:45 +0800

    Optimize: iconfont深浅色优化

--- panels/main_panel.py
+++ panels/main_panel.py
@@ -7,7 +7,7 @@ from panels.train_panel import TrainPanel
 from panels.model_list_panel import ModelListPanel
 from panels.predict_image_panel import PredictImagePanel
 from panels.predict_multi_image_panel import PredictMultiImagePanel
-from panels.predict_video_panel import PredictVideoPanel
+from panels.predict_camera_panel import PredictCameraPanel
 from panels.dedup_panel import DedupPanel
 from panels.delete_model_panel import DeleteModelPanel
 from panels.log_panel import LogPanel
@@ -31,7 +31,7 @@ class MainPanelWindow(QWidget):
         self.btn_models = QPushButton("模型管理")
         self.btn_predict_img = QPushButton("图片预测")
         self.btn_predict_multi_img = QPushButton("多模型图片预测")
-        self.btn_predict_video = QPushButton("视频预测")
+        self.btn_predict_camera = QPushButton("摄像头采集预测")
         self.btn_dedup = QPushButton("数据集去重")
         self.btn_log = QPushButton("查看日志")
         self.btn_theme = QPushButton()
@@ -43,14 +43,14 @@ class MainPanelWindow(QWidget):
         menu_layout.addWidget(self.btn_models)
         menu_layout.addWidget(self.btn_predict_img)
         menu_layout.addWidget(self.btn_predict_multi_img)
-        menu_layout.addWidget(self.btn_predict_video)
+        menu_layout.addWidget(self.btn_predict_camera)
         menu_layout.addWidget(self.btn_dedup)
         menu_layout.addWidget(self.btn_log)
         menu_layout.addStretch()
         menu_layout.addWidget(self.btn_theme)
         for btn in [
             self.btn_train, self.btn_models, self.btn_predict_img, self.btn_predict_multi_img,
-            self.btn_predict_video, self.btn_dedup, self.btn_log
+            self.btn_predict_camera, self.btn_dedup, self.btn_log
         ]:
             btn.setObjectName("menuButton")
 
@@ -59,14 +59,14 @@ class MainPanelWindow(QWidget):
         self.model_list_panel = ModelListPanel()
         self.predict_img_panel = PredictImagePanel()
         self.predict_multi_img_panel = PredictMultiImagePanel()
-        self.predict_video_panel = PredictVideoPanel()
+        self.predict_camera_panel = PredictCameraPanel()
         self.dedup_panel = DedupPanel()
         self.log_panel = LogPanel()
         self.stack.addWidget(self.train_panel)
         self.stack.addWidget(self.model_list_panel)
         self.stack.addWidget(self.predict_img_panel)
         self.stack.addWidget(self.predict_multi_img_panel)
-        self.stack.addWidget(self.predict_video_panel)
+        self.stack.addWidget(self.predict_camera_panel)
         self.stack.addWidget(self.dedup_panel)
         self.stack.addWidget(self.log_panel)
         main_layout.addLayout(menu_layout, 1)
@@ -77,7 +77,7 @@ class MainPanelWindow(QWidget):
         self.btn_models.clicked.connect(lambda: (self.model_list_panel.refresh(), self.switch_panel(1)))
         self.btn_predict_img.clicked.connect(lambda: (self.predict_img_panel.refresh_models(), self.switch_panel(2)))
         self.btn_predict_multi_img.clicked.connect(lambda: (self.predict_img_panel.refresh_models(), self.switch_panel(3)))
-        self.btn_predict_video.clicked.connect(lambda: (self.predict_video_panel.refresh_models(), self.switch_panel(4)))
+        self.btn_predict_camera.clicked.connect(lambda: (self.predict_camera_panel.refresh_models(), self.switch_panel(4)))
         self.btn_dedup.clicked.connect(lambda: self.switch_panel(5))
         self.btn_log.clicked.connect(lambda: (self.log_panel.refresh_run_log(), self.log_panel.refresh_error_log(), self.switch_panel(6)))
         self.btn_theme.clicked.connect(self.toggle_theme)
@@ -93,7 +93,7 @@ class MainPanelWindow(QWidget):
                 self.predict_multi_img_panel.result_text.clear()
                 self.predict_multi_img_panel.refresh_models()
             elif idx == 4:
-                self.predict_video_panel.result_text.clear()
+                self.predict_camera_panel.result_text.clear()
             elif idx == 5:
                 self.dedup_panel.result_text.clear()
         self.stack.setCurrentIndex(idx)
@@ -104,8 +104,16 @@ class MainPanelWindow(QWidget):
         if self.current_theme == "light":
             load_qss(app, DARK_QSS_FILE)
             self.btn_theme.setIcon(QIcon("assets/svg/sun.svg"))
+            self.model_list_panel.btn_refresh.setIcon(QIcon("assets/svg/refresh_dark.svg"))
+            self.model_list_panel.btn_download.setIcon(QIcon("assets/svg/download_dark.svg"))
+            self.log_panel.run_log_refresh_btn.setIcon(QIcon("assets/svg/refresh_dark.svg"))
+            self.log_panel.error_log_refresh_btn.setIcon(QIcon("assets/svg/refresh_dark.svg"))
             self.current_theme = "dark"
         else:
             load_qss(app, LIGHT_QSS_FILE)
             self.btn_theme.setIcon(QIcon("assets/svg/moon.svg"))
+            self.model_list_panel.btn_refresh.setIcon(QIcon("assets/svg/refresh_light.svg"))
+            self.model_list_panel.btn_download.setIcon(QIcon("assets/svg/download_light.svg"))
+            self.log_panel.run_log_refresh_btn.setIcon(QIcon("assets/svg/refresh_light.svg"))
+            self.log_panel.error_log_refresh_btn.setIcon(QIcon("assets/svg/refresh_light.svg"))
             self.current_theme = "light"
